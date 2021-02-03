53a337dc5fa6dd9040df0d3a5dcd8888e628c6e2
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-03 16:56:42 +0100

    Create App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
-import './App.scss'
+import { Game, GameResultsHistory } from '@containers/index'
+import { Music } from '@components/index'
+import styles from './App.scss'
 
 export function App() {
-  return <>Aloha</>
+  return (
+    <>
+      <Music />
+      <div className={styles.gamesContainer}>
+        <Game />
+        <GameResultsHistory />
+      </div>
+    </>
+  )
 }
