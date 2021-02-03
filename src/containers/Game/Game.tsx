7c56761de77df0709305a9307c71126a1c266ca8
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-03 16:58:13 +0100

    Make a game container which holds players info and hands results

---
+++ src/containers/Game/Game.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { UserHandResults, ComputerHandResults, PlayerInfo, SelectContender } from '@components/index'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store/store'
+import styles from './Game.scss'
+
+export const Game: React.FC = () => {
+  const game = useSelector((state: RootState) => state)
+
+  return (
+    <div className={styles.gameContainer}>
+      <div className={styles.firstPlayerContainer}>
+        <PlayerInfo player={game?.game?.players?.user} />
+        <UserHandResults />
+      </div>
+      <SelectContender />
+      <div className={styles.secondPlayerContainer}>
+        <PlayerInfo player={game?.game?.players?.computer} />
+        <ComputerHandResults />
+      </div>
+    </div>
+  )
+}
