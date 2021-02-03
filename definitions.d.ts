fdb7092a8f6223672a1a77314da88414f451c95d
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-03 17:07:29 +0100

    Adding svg for type definition

--- definitions.d.ts
+++ definitions.d.ts
@@ -6,3 +6,7 @@ declare module '*.scss' {
   const content: { [className: string]: string }
   export default content
 }
+declare module '*.svg' {
+  const content: string
+  export default content
+}
