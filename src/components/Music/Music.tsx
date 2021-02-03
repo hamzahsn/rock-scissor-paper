8a74fbec57a885ec35741bbb082e08457b538f4f
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-04 00:32:57 +0100

    Enable Music

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -2,25 +2,19 @@ import React from 'react'
 import styles from './Music.scss'
 
 export const Music = () => {
-  const audio = new Audio('../../../public/music.mp3')
   const [playing, setPlaying] = React.useState(false)
   const toggle = () => setPlaying(!playing)
+
+  const audio = React.useMemo(() => {
+    return new Audio('../../../public/music.mp3')
+  }, [])
 
   React.useEffect(() => {
     playing ? audio.play() : audio.pause()
   }, [playing])
 
-  React.useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false))
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false))
-    }
-  }, [audio])
-
   return (
     <div className={styles.musicWrapper}>
-      {/* <button onClick={playing}>Play</button> */}
-      {/* <button onClick={() => setPlaying(false)}>close</button> */}
       <label className={styles.label}>
         <div className={styles.labelText}>Music</div>
         <div className={styles.toggle}>
