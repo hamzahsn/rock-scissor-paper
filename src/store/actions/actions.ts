f60c47d709bc6956310b2b8f404150b0c76b77ad
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-03 16:49:32 +0100

    Creating actions for the game

---
+++ src/store/actions/actions.ts
@@ -0,0 +1,76 @@
+import {
+  GameActionTypes,
+  SET_USER,
+  START_GAME,
+  FINISH_ROUND,
+  SET_COMPUTER_HAND_RESULT,
+  SET_SCORE,
+  SET_WEAPON,
+  SET_ROUND_NUMBER,
+  SET_PREVIOUS_GAMES,
+  END_GAME
+} from './actionTypes'
+import { calculateWinner } from '../../utils/calculateGameResult'
+
+export function setUser(newUser: string): GameActionTypes {
+  return {
+    type: SET_USER,
+    payload: newUser
+  }
+}
+
+export function setWeapon(weapon: number): GameActionTypes {
+  return {
+    type: SET_WEAPON,
+    payload: weapon
+  }
+}
+
+export function StartPlay(): GameActionTypes {
+  return {
+    type: START_GAME
+  }
+}
+
+export function roundFinish(): GameActionTypes {
+  return {
+    type: FINISH_ROUND
+  }
+}
+
+export function setComputerHandResult(weapon: number): GameActionTypes {
+  return {
+    type: SET_COMPUTER_HAND_RESULT,
+    weapon: weapon
+  }
+}
+
+export function setScore(firstPlayer: number, secondPlayer: number): GameActionTypes {
+  console.log('\x1b[43m firstPlayer ' + firstPlayer)
+  console.log('\x1b[43m secondPlayer ' + secondPlayer)
+  const results = calculateWinner(firstPlayer, secondPlayer)
+  console.log('\x1b[43m results ' + results)
+  console.log('\x1b[43m FROM SET SCORE', 'FIRST PLAYER', firstPlayer, 'SECOND PLAYER', secondPlayer)
+  return {
+    type: SET_SCORE,
+    payload: results
+  }
+}
+
+export function setRoundNumber(): GameActionTypes {
+  return {
+    type: SET_ROUND_NUMBER
+  }
+}
+
+export function setPreviousGames(): GameActionTypes {
+  return {
+    type: SET_PREVIOUS_GAMES
+  }
+}
+
+export function endGame(): GameActionTypes {
+  return {
+    type: END_GAME
+  }
+}
