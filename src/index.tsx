0ab16347c3925a701a84e95da133e4837ee281b6
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-03 17:08:40 +0100

    Wrapping it all inside index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store/store'
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+)
